1f29d5bd52894e618216d1dd9b8aa1a1700d9473
behzad <90694618+behzadiv@users.noreply.github.com>
2023-08-01 17:07:42 +0330

    feat : create html and css structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      
+    },
   },
   plugins: [],
 };
